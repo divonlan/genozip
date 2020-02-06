cac5d501efb466fb9848641780a723aa33d1fbac
Divon Lan <divon@shablife.com>
2020-02-07 00:36:29 +1030

    optimize - avoid copying memory if little endian

--- md5.c
+++ md5.c
@@ -23,8 +23,6 @@ void md5_display_ctx (const Md5Context *x)
 
     printf ("%2u: %08x %08x %08x %08x %08x %08x ", iteration, x->hi, x->lo, x->a, x->b, x->c, x->d);
     for (unsigned i=0; i<64; i++) printf ("%2.2x", x->buffer.bytes[i]);
-    printf (" ");
-    for (unsigned i=0; i<16; i++) printf ("%8.8x", x->block[i]);
     printf ("\n");
 
     iteration++;
@@ -33,9 +31,6 @@ void md5_display_ctx (const Md5Context *x)
 
 static const void *md5_transform (Md5Context *ctx, const void *data, uintmax_t size)
 {
-    #define GET(n) (ctx->block[(n)])
-    #define SET(n) (ctx->block[(n)] = LTEN32(ptr[n]))
-    
     const uint32_t *ptr = (uint32_t *)data;
 
     uint32_t a = ctx->a;
@@ -49,93 +44,98 @@ static const void *md5_transform (Md5Context *ctx, const void *data, uintmax_t s
         uint32_t saved_c = c;
         uint32_t saved_d = d;
 
+        // in little endian machines, we can access the data directly - we don't need to copy memory
+        // in big endian machines - we need to flip the data to little endian - so we do it in a copy
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+        const uint32_t *block = ptr;
+#else
+        uint32_t *block = ctx->block;
+        for (unsigned i=0; i < 16; i++) block[i] = LTEN32(ptr[i]);
+#endif
         // Round 1
-        STEP( F, a, b, c, d, SET(0),  0xd76aa478, 7 )
-        STEP( F, d, a, b, c, SET(1),  0xe8c7b756, 12 )
-        STEP( F, c, d, a, b, SET(2),  0x242070db, 17 )
-        STEP( F, b, c, d, a, SET(3),  0xc1bdceee, 22 )
-        STEP( F, a, b, c, d, SET(4),  0xf57c0faf, 7 )
-        STEP( F, d, a, b, c, SET(5),  0x4787c62a, 12 )
-        STEP( F, c, d, a, b, SET(6),  0xa8304613, 17 )
-        STEP( F, b, c, d, a, SET(7),  0xfd469501, 22 )
-        STEP( F, a, b, c, d, SET(8),  0x698098d8, 7 )
-        STEP( F, d, a, b, c, SET(9),  0x8b44f7af, 12 )
-        STEP( F, c, d, a, b, SET(10), 0xffff5bb1, 17 )
-        STEP( F, b, c, d, a, SET(11), 0x895cd7be, 22 )
-        STEP( F, a, b, c, d, SET(12), 0x6b901122, 7 )
-        STEP( F, d, a, b, c, SET(13), 0xfd987193, 12 )
-        STEP( F, c, d, a, b, SET(14), 0xa679438e, 17 )
-        STEP( F, b, c, d, a, SET(15), 0x49b40821, 22 )
+        STEP( F, a, b, c, d, block[0],  0xd76aa478, 7  )
+        STEP( F, d, a, b, c, block[1],  0xe8c7b756, 12 )
+        STEP( F, c, d, a, b, block[2],  0x242070db, 17 )
+        STEP( F, b, c, d, a, block[3],  0xc1bdceee, 22 )
+        STEP( F, a, b, c, d, block[4],  0xf57c0faf, 7  )
+        STEP( F, d, a, b, c, block[5],  0x4787c62a, 12 )
+        STEP( F, c, d, a, b, block[6],  0xa8304613, 17 )
+        STEP( F, b, c, d, a, block[7],  0xfd469501, 22 )
+        STEP( F, a, b, c, d, block[8],  0x698098d8, 7  )
+        STEP( F, d, a, b, c, block[9],  0x8b44f7af, 12 )
+        STEP( F, c, d, a, b, block[10], 0xffff5bb1, 17 )
+        STEP( F, b, c, d, a, block[11], 0x895cd7be, 22 )
+        STEP( F, a, b, c, d, block[12], 0x6b901122, 7  )
+        STEP( F, d, a, b, c, block[13], 0xfd987193, 12 )
+        STEP( F, c, d, a, b, block[14], 0xa679438e, 17 )
+        STEP( F, b, c, d, a, block[15], 0x49b40821, 22 )
 
         // Round 2
-        STEP( G, a, b, c, d, GET(1),  0xf61e2562, 5 )
-        STEP( G, d, a, b, c, GET(6),  0xc040b340, 9 )
-        STEP( G, c, d, a, b, GET(11), 0x265e5a51, 14 )
-        STEP( G, b, c, d, a, GET(0),  0xe9b6c7aa, 20 )
-        STEP( G, a, b, c, d, GET(5),  0xd62f105d, 5 )
-        STEP( G, d, a, b, c, GET(10), 0x02441453, 9 )
-        STEP( G, c, d, a, b, GET(15), 0xd8a1e681, 14 )
-        STEP( G, b, c, d, a, GET(4),  0xe7d3fbc8, 20 )
-        STEP( G, a, b, c, d, GET(9),  0x21e1cde6, 5 )
-        STEP( G, d, a, b, c, GET(14), 0xc33707d6, 9 )
-        STEP( G, c, d, a, b, GET(3),  0xf4d50d87, 14 )
-        STEP( G, b, c, d, a, GET(8),  0x455a14ed, 20 )
-        STEP( G, a, b, c, d, GET(13), 0xa9e3e905, 5 )
-        STEP( G, d, a, b, c, GET(2),  0xfcefa3f8, 9 )
-        STEP( G, c, d, a, b, GET(7),  0x676f02d9, 14 )
-        STEP( G, b, c, d, a, GET(12), 0x8d2a4c8a, 20 )
+        STEP( G, a, b, c, d, block[1],  0xf61e2562, 5  )
+        STEP( G, d, a, b, c, block[6],  0xc040b340, 9  )
+        STEP( G, c, d, a, b, block[11], 0x265e5a51, 14 )
+        STEP( G, b, c, d, a, block[0],  0xe9b6c7aa, 20 )
+        STEP( G, a, b, c, d, block[5],  0xd62f105d, 5  )
+        STEP( G, d, a, b, c, block[10], 0x02441453, 9  )
+        STEP( G, c, d, a, b, block[15], 0xd8a1e681, 14 )
+        STEP( G, b, c, d, a, block[4],  0xe7d3fbc8, 20 )
+        STEP( G, a, b, c, d, block[9],  0x21e1cde6, 5  )
+        STEP( G, d, a, b, c, block[14], 0xc33707d6, 9  )
+        STEP( G, c, d, a, b, block[3],  0xf4d50d87, 14 )
+        STEP( G, b, c, d, a, block[8],  0x455a14ed, 20 )
+        STEP( G, a, b, c, d, block[13], 0xa9e3e905, 5  )
+        STEP( G, d, a, b, c, block[2],  0xfcefa3f8, 9  )
+        STEP( G, c, d, a, b, block[7],  0x676f02d9, 14 )
+        STEP( G, b, c, d, a, block[12], 0x8d2a4c8a, 20 )
 
         // Round 3
-        STEP( H, a, b, c, d, GET(5),  0xfffa3942, 4 )
-        STEP( H, d, a, b, c, GET(8),  0x8771f681, 11 )
-        STEP( H, c, d, a, b, GET(11), 0x6d9d6122, 16 )
-        STEP( H, b, c, d, a, GET(14), 0xfde5380c, 23 )
-        STEP( H, a, b, c, d, GET(1),  0xa4beea44, 4 )
-        STEP( H, d, a, b, c, GET(4),  0x4bdecfa9, 11 )
-        STEP( H, c, d, a, b, GET(7),  0xf6bb4b60, 16 )
-        STEP( H, b, c, d, a, GET(10), 0xbebfbc70, 23 )
-        STEP( H, a, b, c, d, GET(13), 0x289b7ec6, 4 )
-        STEP( H, d, a, b, c, GET(0),  0xeaa127fa, 11 )
-        STEP( H, c, d, a, b, GET(3),  0xd4ef3085, 16 )
-        STEP( H, b, c, d, a, GET(6),  0x04881d05, 23 )
-        STEP( H, a, b, c, d, GET(9),  0xd9d4d039, 4 )
-        STEP( H, d, a, b, c, GET(12), 0xe6db99e5, 11 )
-        STEP( H, c, d, a, b, GET(15), 0x1fa27cf8, 16 )
-        STEP( H, b, c, d, a, GET(2),  0xc4ac5665, 23 )
+        STEP( H, a, b, c, d, block[5],  0xfffa3942, 4  )
+        STEP( H, d, a, b, c, block[8],  0x8771f681, 11 )
+        STEP( H, c, d, a, b, block[11], 0x6d9d6122, 16 )
+        STEP( H, b, c, d, a, block[14], 0xfde5380c, 23 )
+        STEP( H, a, b, c, d, block[1],  0xa4beea44, 4  )
+        STEP( H, d, a, b, c, block[4],  0x4bdecfa9, 11 )
+        STEP( H, c, d, a, b, block[7],  0xf6bb4b60, 16 )
+        STEP( H, b, c, d, a, block[10], 0xbebfbc70, 23 )
+        STEP( H, a, b, c, d, block[13], 0x289b7ec6, 4  )
+        STEP( H, d, a, b, c, block[0],  0xeaa127fa, 11 )
+        STEP( H, c, d, a, b, block[3],  0xd4ef3085, 16 )
+        STEP( H, b, c, d, a, block[6],  0x04881d05, 23 )
+        STEP( H, a, b, c, d, block[9],  0xd9d4d039, 4  )
+        STEP( H, d, a, b, c, block[12], 0xe6db99e5, 11 )
+        STEP( H, c, d, a, b, block[15], 0x1fa27cf8, 16 )
+        STEP( H, b, c, d, a, block[2],  0xc4ac5665, 23 )
 
         // Round 4
-        STEP( I, a, b, c, d, GET(0),  0xf4292244, 6 )
-        STEP( I, d, a, b, c, GET(7),  0x432aff97, 10 )
-        STEP( I, c, d, a, b, GET(14), 0xab9423a7, 15 )
-        STEP( I, b, c, d, a, GET(5),  0xfc93a039, 21 )
-        STEP( I, a, b, c, d, GET(12), 0x655b59c3, 6 )
-        STEP( I, d, a, b, c, GET(3),  0x8f0ccc92, 10 )
-        STEP( I, c, d, a, b, GET(10), 0xffeff47d, 15 )
-        STEP( I, b, c, d, a, GET(1),  0x85845dd1, 21 )
-        STEP( I, a, b, c, d, GET(8),  0x6fa87e4f, 6 )
-        STEP( I, d, a, b, c, GET(15), 0xfe2ce6e0, 10 )
-        STEP( I, c, d, a, b, GET(6),  0xa3014314, 15 )
-        STEP( I, b, c, d, a, GET(13), 0x4e0811a1, 21 )
-        STEP( I, a, b, c, d, GET(4),  0xf7537e82, 6 )
-        STEP( I, d, a, b, c, GET(11), 0xbd3af235, 10 )
-        STEP( I, c, d, a, b, GET(2),  0x2ad7d2bb, 15 )
-        STEP( I, b, c, d, a, GET(9),  0xeb86d391, 21 )
+        STEP( I, a, b, c, d, block[0],  0xf4292244, 6  )
+        STEP( I, d, a, b, c, block[7],  0x432aff97, 10 )
+        STEP( I, c, d, a, b, block[14], 0xab9423a7, 15 )
+        STEP( I, b, c, d, a, block[5],  0xfc93a039, 21 )
+        STEP( I, a, b, c, d, block[12], 0x655b59c3, 6  )
+        STEP( I, d, a, b, c, block[3],  0x8f0ccc92, 10 )
+        STEP( I, c, d, a, b, block[10], 0xffeff47d, 15 )
+        STEP( I, b, c, d, a, block[1],  0x85845dd1, 21 )
+        STEP( I, a, b, c, d, block[8],  0x6fa87e4f, 6  )
+        STEP( I, d, a, b, c, block[15], 0xfe2ce6e0, 10 )
+        STEP( I, c, d, a, b, block[6],  0xa3014314, 15 )
+        STEP( I, b, c, d, a, block[13], 0x4e0811a1, 21 )
+        STEP( I, a, b, c, d, block[4],  0xf7537e82, 6  )
+        STEP( I, d, a, b, c, block[11], 0xbd3af235, 10 )
+        STEP( I, c, d, a, b, block[2],  0x2ad7d2bb, 15 )
+        STEP( I, b, c, d, a, block[9],  0xeb86d391, 21 )
 
-        a += saved_a;
+        a += saved_a;   
         b += saved_b;
         c += saved_c;
         d += saved_d;
 
         ptr += 16;
-    } while( size -= 64 );
+    } while (size -= 64);
 
     ctx->a = a;
     ctx->b = b;
     ctx->c = c;
     ctx->d = d;
-
-    #undef GET
-    #undef SET
 
     return ptr;
 }
