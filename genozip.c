68b3449774c4e9c949c9cc4530fb8e17d0b2d091
Divon Lan <divon@shablife.com>
2020-03-06 09:26:45 +1030

    fix

--- genozip.c
+++ genozip.c
@@ -504,7 +504,7 @@ static void main_test_after_genozip (char *exec_name, char *z_filename)
     
     int *wstatus;
     wait (wstatus); // I am the parent - wait for child, so that the terminal doesn't print the prompt until the child is done
-    if (wstatus) fprintf (stderr, "genozip test exited with status %u\n", wstatus;)
+    if (wstatus) fprintf (stderr, "genozip test exited with status %u\n", wstatus)
 #endif
 }
 
