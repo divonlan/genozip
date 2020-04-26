8d87959053def748785d5c0f93351d7cd9967c67
Divon Lan <divon@shablife.com>
2020-04-26 19:17:37 +0930

    update

--- text_help.h
+++ text_help.h
@@ -254,7 +254,7 @@ static const char *help_genocat[] = {
     "                     Note: sample names are case-sensitive",
     "                     Note: Multiple -s arguments may be specified - this is equivalent to chaining their samples with a comma separator in a single argument",
     "",
-    "   -g --grep         <string> Show only records where <string> is a case-sensitive substring of the description",
+    "   -g --grep         <string> Show only records in which <string> is a case-sensitive substring of the description",
     "   FASTQ",
     "",
     "   -G --drop-genotypes Output the data without the individual genotypes and FORMAT column",
