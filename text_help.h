6c9ef3adfe4d93a20e5192c93d6c1b846c727725
Divon Lan <divon@shablife.com>
2020-05-05 11:27:44 +0930

    don't allow --strip on VCF files with END INFO

--- text_help.h
+++ text_help.h
@@ -283,7 +283,6 @@ static const char *help_genocat[] = {
     "   FASTA",
     "",
     "   --strip           Display only genotype / sequence data and minimal additional information",
-    "                     Limitation: should not be used with a VCF file that has an END subfield in INFO",
     "",
     "   -o --output       <output -filename>. Output to this filename instead of stdout",
     "",
