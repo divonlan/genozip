15defdad2b499c28dec2aa6901d920cdf3e12670
Divon Lan <divon@shablife.com>
2020-05-29 18:48:20 +0930

    cleanup

--- sam_piz.c
+++ sam_piz.c
@@ -66,70 +66,13 @@ void sam_piz_special_BI (VBlock *vb, MtfContext *ctx, const char *snip, unsigned
     ctx->next_local  += vb->seq_len;
 }
 
-static void sam_piz_map_optional_subfields (VBlockSAM *vb)
-{
-    // terminology: we call a list of INFO subfield names, an "oname". An oname looks something like
-    // this: "MX:Z:ab:i:". Each oname consists of OPTIONAL subfields. These subfields are not unique to this
-    // oname and can appear in other onames. Each optional subfield is made of a segment of the template eg "MX:Z:"
-    // and a value eg "abcded" which is stored in the b250 of that optional subfield.
-
-    const MtfContext *optional_ctx = &vb->contexts[SAM_OPTIONAL];
-    vb->optional_mapper_buf.len = optional_ctx->word_list.len;
-    buf_alloc (vb, &vb->optional_mapper_buf, sizeof (SubfieldMapper) * vb->optional_mapper_buf.len,
-               1, "optional_mapper_buf", 0);
-    buf_zero (&vb->optional_mapper_buf);
-
-    ARRAY (SubfieldMapper, all_optional_mappers, vb->optional_mapper_buf);
-
-    ARRAY (const MtfWord, all_optionals, optional_ctx->word_list);
-
-    for (unsigned optional_i=0; optional_i < vb->optional_mapper_buf.len; optional_i++) {
-
-        char *optional = ENT (char, optional_ctx->dict, all_optionals[optional_i].char_index); // e.g. "MX:Z:ab:i:" - pointer into the OPTIONAL dictionary
-        SubfieldMapper *optional_mapper = &all_optional_mappers[optional_i]; // optional_mapper of this specific set of names "I1=I2=I3="
-        optional_mapper->num_subfields = all_optionals[optional_i].snip_len / 5; 
-
-        // traverse the subfields of one optional. E.g. if the optional is "MX:Z:ab:i:" then we traverse MX, ab
-        // note - optional 
-        for (unsigned i=0; i < optional_mapper->num_subfields; i++) {
-                        
-            DictIdType dict_id = sam_dict_id_optnl_sf (dict_id_make (&optional[i*5], 4)); // get dict_id from first 4 characters eg "MX:i"
-            optional_mapper->did_i[i] = mtf_get_ctx (vb, dict_id)->did_i; 
-        }
-    }
-}
-/*
-static void sam_piz_reconstruct_optional_fields (VBlockSAM *vb)
-{
-    DECLARE_SNIP;
-    uint32_t opt_word_index = LOAD_SNIP (SAM_OPTIONAL);
-
-    SubfieldMapper *opt_map = ENT (SubfieldMapper, vb->optional_mapper_buf, opt_word_index);
-
-    ASSERT (opt_map->num_subfields == snip_len / 5, "Error: opt_map->num_subfields=%u but snip=%.*s indicates %u optional fields. sam_line=%u", 
-            opt_map->num_subfields, snip_len, snip, snip_len/5, vb->line_i);
-
-    for (unsigned sf_i=0; sf_i < opt_map->num_subfields; sf_i++) {
-
-        RECONSTRUCT (&snip[sf_i*5], 5)
-
-        uint8_t did_i = opt_map->did_i[sf_i];
-        piz_reconstruct_from_ctx (vb, did_i, 0);        
-
-        if (sf_i != opt_map->num_subfields-1)
-            RECONSTRUCT1 ('\t');
-    }
-}
-*/
 void sam_piz_reconstruct_vb (VBlockSAM *vb)
 {
     piz_map_compound_field ((VBlockP)vb, sam_dict_id_is_qname_sf, &vb->qname_mapper);
-    sam_piz_map_optional_subfields (vb);
 
-    for (uint32_t vb_line_i=0; vb_line_i < vb->lines.len; vb_line_i++) {
+    for (vb->line_i=vb->first_line; vb->line_i < vb->first_line + vb->lines.len; vb->line_i++) {
 
         uint32_t txt_data_start = vb->txt_data.len;
-        vb->line_i = vb->first_line + vb_line_i;
 
         piz_reconstruct_from_ctx (vb, SAM_QNAME,    '\t');
         piz_reconstruct_from_ctx (vb, SAM_FLAG,     '\t');
@@ -142,12 +85,13 @@ void sam_piz_reconstruct_vb (VBlockSAM *vb)
         piz_reconstruct_from_ctx (vb, SAM_TLEN,     '\t');
         piz_reconstruct_from_ctx (vb, SAM_SEQ,      '\t');
         piz_reconstruct_from_ctx (vb, SAM_QUAL,     '\t');
-        piz_reconstruct_from_ctx (vb, SAM_OPTIONAL, 0); // the subfields provide the \t separators
+        piz_reconstruct_from_ctx (vb, SAM_OPTIONAL, 0   ); // the optional subfields (if there are any) provide the \t separators
 
-        vb->txt_data.len--; // remove last \t
+        vb->txt_data.len--; // remove last \t (the line has ended either after QUAL or after the last OPTIONAL subfield)
         piz_reconstruct_from_ctx (vb, SAM_EOL, 0);
 
         // after consuming the line's data, if it is not to be outputted - trim txt_data back to start of line
         if (vb->dont_show_curr_line) vb->txt_data.len = txt_data_start; 
     }
 }
+ 
