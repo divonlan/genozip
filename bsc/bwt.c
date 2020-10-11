ee5159e8f1134d199cd290866b24eccdf4fca634
Divon Lan <divon@shablife.com>
2020-10-11 20:23:15 +1030

    move some buffers from heap to stack

--- bsc/bwt.c
+++ bsc/bwt.c
@@ -45,7 +45,7 @@ See also the bsc and libbsc web site:
 
 int bsc_bwt_encode(void *vb, unsigned char * T, int n, unsigned char * num_indexes, int * indexes, int features)
 {
-    int index = divbwt(vb, T, T, NULL, n, num_indexes, indexes, 0);
+    int index = divbwt (vb, T, T, NULL, n, num_indexes, indexes, 0);
     switch (index)
     {
         case -1 : return LIBBSC_BAD_PARAMETER;
@@ -56,9 +56,7 @@ int bsc_bwt_encode(void *vb, unsigned char * T, int n, unsigned char * num_index
 
 static int bsc_unbwt_mergedTL_serial(void *vb, unsigned char * restrict T, unsigned int * restrict P, int n, int index)
 {
-    unsigned int bucket[ALPHABET_SIZE];
-
-    memset(bucket, 0, ALPHABET_SIZE * sizeof(unsigned int));
+    unsigned int bucket[ALPHABET_SIZE] = { 0 };
 
     for (int i = 0; i < index; ++i)
     {
@@ -90,90 +88,81 @@ static int bsc_unbwt_mergedTL_serial(void *vb, unsigned char * restrict T, unsig
 
 static int bsc_unbwt_biPSI_serial(void *vb, unsigned char * restrict T, unsigned int * restrict P, int n, int index)
 {
-    int * restrict bucket = (int *)bsc_zero_malloc (vb, ALPHABET_SIZE * ALPHABET_SIZE * sizeof(int));
-    if (bucket)
+    int bucket[ALPHABET_SIZE * ALPHABET_SIZE] = { 0 };
+    unsigned short fastbits[1 + (1 << BWT_NUM_FASTBITS)];
+    int count[ALPHABET_SIZE] = { 0 };
+
+    int shift = 0; while ((n >> shift) > (1 << BWT_NUM_FASTBITS)) shift++;
+
+    for (int i = 0; i < n; ++i) count[T[i]]++;
+
+    for (int sum = 1, c = 0; c < ALPHABET_SIZE; ++c)
     {
-        unsigned short * restrict fastbits = (unsigned short *)bsc_malloc (vb, ((1 + (1 << BWT_NUM_FASTBITS)) * sizeof(unsigned short)));
-        if (fastbits)
+        int tmp = sum; sum += count[c]; count[c] = tmp;
+        if (count[c] != sum)
         {
-            int count[ALPHABET_SIZE]; memset(count, 0, ALPHABET_SIZE * sizeof(int));
+            int * restrict bucket_p = &bucket[c << 8];
 
-            int shift = 0; while ((n >> shift) > (1 << BWT_NUM_FASTBITS)) shift++;
+            int hi = index; if (sum < hi) hi = sum;
+            for (int i = count[c]; i < hi; ++i) bucket_p[T[i]]++;
 
-            for (int i = 0; i < n; ++i) count[T[i]]++;
-
-            for (int sum = 1, c = 0; c < ALPHABET_SIZE; ++c)
-            {
-                int tmp = sum; sum += count[c]; count[c] = tmp;
-                if (count[c] != sum)
-                {
-                    int * restrict bucket_p = &bucket[c << 8];
-
-                    int hi = index; if (sum < hi) hi = sum;
-                    for (int i = count[c]; i < hi; ++i) bucket_p[T[i]]++;
-
-                    int lo = index + 1; if (count[c] > lo) lo = count[c];
-                    for (int i = lo; i < sum; ++i) bucket_p[T[i - 1]]++;
-                }
-            }
-
-            int lastc = T[0];
-            for (int v = 0, sum = 1, c = 0; c < ALPHABET_SIZE; ++c)
-            {
-                if (c == lastc) sum++;
-
-                int * restrict bucket_p = &bucket[c];
-                for (int d = 0; d < ALPHABET_SIZE; ++d)
-                {
-                    int tmp = sum; sum += bucket_p[d << 8]; bucket_p[d << 8] = tmp;
-                    if (bucket_p[d << 8] != sum)
-                    {
-                        for (; v <= ((sum - 1) >> shift); ++v) fastbits[v] = (c << 8) | d;
-                    }
-                }
-            }
-
-            for (int i = 0; i < index; ++i)
-            {
-                unsigned char c = T[i];
-                int           p = count[c]++;
-
-                if (p < index) P[bucket[(c << 8) | T[p    ]]++] = i; else
-                if (p > index) P[bucket[(c << 8) | T[p - 1]]++] = i;
-            }
-
-            for (int i = index; i < n; ++i)
-            {
-                unsigned char c = T[i];
-                int           p = count[c]++;
-
-                if (p < index) P[bucket[(c << 8) | T[p    ]]++] = i + 1; else
-                if (p > index) P[bucket[(c << 8) | T[p - 1]]++] = i + 1;
-            }
-
-            for (int c = 0; c < ALPHABET_SIZE; ++c)
-            {
-                for (int d = 0; d < c; ++d)
-                {
-                    int tmp = bucket[(d << 8) | c]; bucket[(d << 8) | c] = bucket[(c << 8) | d]; bucket[(c << 8) | d] = tmp;
-                }
-            }
-
-            for (int p = index, i = 1; i < n; i += 2)
-            {
-                int c = fastbits[p >> shift]; while (bucket[c] <= p) c++;
-                T[i - 1] = (unsigned char)(c >> 8); T[i] = (unsigned char)(c & 0xff);
-                p = P[p];
-            }
-
-            T[n - 1] = (unsigned char)lastc;
-
-            bsc_free (vb, fastbits); bsc_free (vb, bucket);
-            return LIBBSC_NO_ERROR;
+            int lo = index + 1; if (count[c] > lo) lo = count[c];
+            for (int i = lo; i < sum; ++i) bucket_p[T[i - 1]]++;
         }
-        bsc_free (vb, bucket);
-    };
-    return LIBBSC_NOT_ENOUGH_MEMORY;
+    }
+
+    int lastc = T[0];
+    for (int v = 0, sum = 1, c = 0; c < ALPHABET_SIZE; ++c)
+    {
+        if (c == lastc) sum++;
+
+        int * restrict bucket_p = &bucket[c];
+        for (int d = 0; d < ALPHABET_SIZE; ++d)
+        {
+            int tmp = sum; sum += bucket_p[d << 8]; bucket_p[d << 8] = tmp;
+            if (bucket_p[d << 8] != sum)
+            {
+                for (; v <= ((sum - 1) >> shift); ++v) fastbits[v] = (c << 8) | d;
+            }
+        }
+    }
+
+    for (int i = 0; i < index; ++i)
+    {
+        unsigned char c = T[i];
+        int           p = count[c]++;
+
+        if (p < index) P[bucket[(c << 8) | T[p    ]]++] = i; else
+        if (p > index) P[bucket[(c << 8) | T[p - 1]]++] = i;
+    }
+
+    for (int i = index; i < n; ++i)
+    {
+        unsigned char c = T[i];
+        int           p = count[c]++;
+
+        if (p < index) P[bucket[(c << 8) | T[p    ]]++] = i + 1; else
+        if (p > index) P[bucket[(c << 8) | T[p - 1]]++] = i + 1;
+    }
+
+    for (int c = 0; c < ALPHABET_SIZE; ++c)
+    {
+        for (int d = 0; d < c; ++d)
+        {
+            int tmp = bucket[(d << 8) | c]; bucket[(d << 8) | c] = bucket[(c << 8) | d]; bucket[(c << 8) | d] = tmp;
+        }
+    }
+
+    for (int p = index, i = 1; i < n; i += 2)
+    {
+        int c = fastbits[p >> shift]; while (bucket[c] <= p) c++;
+        T[i - 1] = (unsigned char)(c >> 8); T[i] = (unsigned char)(c & 0xff);
+        p = P[p];
+    }
+
+    T[n - 1] = (unsigned char)lastc;
+
+    return LIBBSC_NO_ERROR;
 }
 
 static int bsc_unbwt_reconstruct_serial(void *vb, unsigned char * T, unsigned int * P, int n, int index)
