a1548a93243eea9042ccc224b615899aec1a900e
Divon Lan <divon@shablife.com>
2021-07-02 16:40:33 +0930

    update

---
+++ genols.h
@@ -0,0 +1,11 @@
+// ------------------------------------------------------------------
+//   genols.h
+//   Copyright (C) 2019-2021 Divon Lan <divon@genozip.com>
+//   Please see terms and conditions in the files LICENSE.non-commercial.txt and LICENSE.commercial.txt
+
+#ifndef GENOLS_INCLUDED
+#define GENOLS_INCLUDED
+
+extern void genols (const char *z_filename, bool finalize, const char *subdir, bool recursive);
+
+#endif
