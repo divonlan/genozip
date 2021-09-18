b21257c8a64fd09cdebcce0c7ec8455715b76a82
Divon Lan <divon@shablife.com>
2021-09-18 12:50:27 +0930

    fix gcc-10 compilation issues

--- bsc/platform.h
+++ bsc/platform.h
@@ -3,10 +3,15 @@
 //   Copyright (C) 2019-2021 Black Paw Ventures Limited
 //   Please see terms and conditions in the file LICENSE.txt
 
+#ifndef BSC_PLATFORM_INCLUDED
+#define BSC_PLATFORM_INCLUDED
+
 #include "../genozip.h"
 
 #define ALPHABET_SIZE 256
 
-void *(* bsc_malloc)(void *vb, size_t size);
-void  (* bsc_free)(void *vb, void *address);
-void *bsc_zero_malloc (void *vb, size_t size);
+extern void *(* bsc_malloc)(void *vb, size_t size);
+extern void  (* bsc_free)(void *vb, void *address);
+extern void *bsc_zero_malloc (void *vb, size_t size);
+
+#endif
