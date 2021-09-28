21aab49474fc61ca931e51bebd9d5060e9cb9cce
Divon Lan <divon@shablife.com>
2021-09-28 18:17:51 +0930

    build docs

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2019-2021, Divon Lan'
 author = 'Divon Lan'
 
 # The full version, including alpha/beta/rc tags
-release = '12.0.36'
+release = '12.0.37'
 
 
 # -- General configuration ---------------------------------------------------
