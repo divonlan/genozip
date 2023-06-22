32c71738a97ac90db273f3709c9a2c7a13878c7a
Divon Lan <divon@shablife.com>
2023-06-22 23:29:23 +0300

    15.0.4

--- src/buf_struct.c
+++ src/buf_struct.c
@@ -692,6 +692,8 @@ void buf_low_level_free (void *p, FUNCLINE)
 
 void *buf_low_level_realloc (void *p, size_t size, rom name, FUNCLINE)
 {
+    uint64_t p_int = (uint64_t)p; // avoid compiler warning for printing dangling pointer
+
 #ifndef _WIN32
     void *new = realloc (p, size);
 #else
@@ -702,8 +704,7 @@ void *buf_low_level_realloc (void *p, size_t size, rom name, FUNCLINE)
              IS_ZIP ? "Try limiting the number of concurrent threads with --threads (affects speed) or reducing the amount of data processed by each thread with --vblock (affects compression ratio)" : "");
 
     if (flag.debug_memory && size >= flag.debug_memory) 
-        iprintf ("realloc(): old=0x%"PRIx64" new=%p name=%s size=%"PRIu64" %s:%u\n", // note: don't use %p with p, to avoid compiler "dangling pointer" warning
-                 (uint64_t)p, new, name, (uint64_t)size, func, code_line);
+        iprintf ("realloc(): old=0x%"PRIx64" new=%p name=%s size=%"PRIu64" %s:%u\n", p_int, new, name, (uint64_t)size, func, code_line);
 
     return new;
 }
