46ddfa656890528a0ff6014baae5047ea4f30487
Divon Lan <divon@shablife.com>
2023-09-18 13:22:33 +0300

    store AD items as int

--- src/vcf_samples.c
+++ src/vcf_samples.c
@@ -208,7 +208,7 @@ static inline bool vcf_seg_sample_has_null_value (uint64_t dnum, ContextP *ctxs,
 
 // returns: 0,1,2 correspond to 0/0 0/1 1/1, 3 means "no dosage" - multi-allelic or '.' or ploidy > 2
 // -1 means caller should not use her SPECIAL
-int vcf_seg_get_mux_channel_i (VBlockVCFP vb, bool fail_if_dvcf_refalt_switch)
+static int vcf_mux_by_dosage_channel_i (VBlockVCFP vb, bool fail_if_dvcf_refalt_switch)
 {
     // fail if this is a DVCF ref<>alt switch, if caller requested so,
     // or if there is no valid GT in this sample
@@ -227,7 +227,7 @@ int vcf_seg_get_mux_channel_i (VBlockVCFP vb, bool fail_if_dvcf_refalt_switch)
 // if cell is NULL, leaves it up to the caller to seg to the channel 
 ContextP vcf_seg_FORMAT_mux_by_dosage (VBlockVCFP vb, ContextP ctx, STRp(cell), const DosageMultiplexer *mux) 
 {
-    int channel_i = vcf_seg_get_mux_channel_i (vb, true);
+    int channel_i = vcf_mux_by_dosage_channel_i (vb, true);
 
     // we don't use the multiplexer if its a DVCF REF⇆ALT switch variant as GT changes
     if (channel_i == -1) {
@@ -248,6 +248,25 @@ ContextP vcf_seg_FORMAT_mux_by_dosage (VBlockVCFP vb, ContextP ctx, STRp(cell), 
     seg_by_ctx (VB, STRa(mux->snip), ctx, 0);
 
     return channel_ctx;
+}
+
+static void vcf_seg_FORMAT_mux_by_dosage_int (VBlockVCFP vb, ContextP ctx, int64_t value, const DosageMultiplexer *mux, uint32_t add_bytes) 
+{
+    int channel_i = vcf_mux_by_dosage_channel_i (vb, true);
+
+    // we don't use the multiplexer if its a DVCF REF⇆ALT switch variant as GT changes
+    if (channel_i == -1) {
+        seg_integer_as_snip_do (VB, ctx, value, add_bytes);
+        return;
+    }
+
+    ContextP channel_ctx = seg_mux_get_channel_ctx (VB, ctx->did_i, MUX, channel_i);
+    
+    channel_ctx->ltype = LT_DYN_INT;
+    seg_integer (VB, channel_ctx, value, true, add_bytes);
+
+    // note: this is not necessarily all-the-same - there could be unmuxed snips due to REF⇆ALT switch, and/or WORD_INDEX_MISSING 
+    seg_by_ctx (VB, STRa(mux->snip), ctx, 0);
 }
 
 // mirroring seg, we accept monoploid or diploid genotypes, with alleles 0 and 1.
@@ -285,7 +304,7 @@ static void vcf_seg_FORMAT_mux_by_dosagexDP (VBlockVCFP vb, ContextP ctx, STRp(c
     if (!str_get_int (STRlst (FORMAT_DP), &DP)) // In some files, DP may be '.'
         DP=0;
 
-    int channel_i = vcf_seg_get_mux_channel_i (vb, true); // we don't use the multiplexer if its a DVCF REF⇆ALT switch variant as GT changes
+    int channel_i = vcf_mux_by_dosage_channel_i (vb, true); // we don't use the multiplexer if its a DVCF REF⇆ALT switch variant as GT changes
     if (channel_i == -1) goto cannot_use_special;
 
     unsigned num_dps = mux->num_channels / 3;
@@ -469,10 +488,10 @@ static void vcf_seg_AD_items (VBlockVCFP vb, ContextP ctx, STRps(item), ContextP
                 vcf_seg_FORMAT_transposed (vb, item_ctxs[0], STRi(item, 0), item_lens[0]);
             
             else if (i==0 || i==1) {
-                if (!vb->mux_AD[i].num_channels)
+                if (!vb->mux_AD[i].num_channels) 
                     seg_mux_init (VB, item_ctxs[i], 4, VCF_SPECIAL_MUX_BY_DOSAGE, false, (MultiplexerP)&vb->mux_AD[i]);
-                
-                vcf_seg_FORMAT_mux_by_dosage (vb, item_ctxs[i], STRi(item, i), &vb->mux_AD[i]);
+
+                vcf_seg_FORMAT_mux_by_dosage_int (vb, item_ctxs[i], values[i], &vb->mux_AD[i], item_lens[i]);
             }
             else
                 seg_by_ctx (VB, STRi(item, i), item_ctxs[i], item_lens[i]);
@@ -1107,7 +1126,7 @@ static bool vcf_phred_optimize (rom snip, unsigned len, char *optimized_snip, un
 // If expectation is met, SPECIAL is segged in AB. 
 static inline void vcf_seg_FORMAT_AB (VBlockVCFP vb, ContextP ctx, STRp(ab))
 {
-    int channel_i = vcf_seg_get_mux_channel_i (vb, false);
+    int channel_i = vcf_mux_by_dosage_channel_i (vb, false);
     bool is_0_or_2 = (channel_i==0 || channel_i==2); // note: dos02 doesn't change in case of DVCF REF⇆ALT switch
     bool ab_missing = ab_len==1 && *ab=='.';
 
