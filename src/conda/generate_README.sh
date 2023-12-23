ff34389215d1c96f76dc440b4d39bb2f1cd068fe
Divon Lan <divon@shablife.com>
2023-12-23 17:09:19 +0200

    15.0.33

--- src/conda/generate_README.sh
+++ src/conda/generate_README.sh
@@ -3,7 +3,7 @@
 # This script is invoked by Makefile
 
 (  sed /__README_MD__/Q conda/README.template.md ; \
-   ./html-to-md.sed README.md | grep -v "<!" ; \
+   ./html-to-md.sed ../README.md | grep -v "<!" ; \
    sed '0,/__README_MD__/d' conda/README.template.md \
    ) | \
    grep -v "^#" 
--- src/version.h
+++ src/version.h
@@ -1,4 +1,4 @@
-#define GENOZIP_CODE_VERSION "15.0.32"
+#define GENOZIP_CODE_VERSION "15.0.33"
 
 extern int code_version_major (void);
 extern int code_version_minor (void);
