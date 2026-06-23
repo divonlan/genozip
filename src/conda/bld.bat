7c60e2794e7b7e51e079796327232295627e7aa2
Divon Lan <divon@shablife.com>
2026-06-23 09:56:45 +0300

    update

--- src/conda/bld.bat
+++ src/conda/bld.bat
@@ -1,6 +1,11 @@
-make OS=Windows_NT all 
+make OS=Windows_NT all
 
-copy src/genozip.exe %LIBRARY_BIN%
-copy src/genounzip.exe %LIBRARY_BIN%
-copy src/genols.exe %LIBRARY_BIN%
-copy src/genocat.exe %LIBRARY_BIN%
+if not exist "%LIBRARY_BIN%" mkdir "%LIBRARY_BIN%"
+
+copy src\genozip.exe   "%LIBRARY_BIN%"
+copy src\genounzip.exe "%LIBRARY_BIN%"
+copy src\genols.exe    "%LIBRARY_BIN%"
+copy src\genocat.exe   "%LIBRARY_BIN%"
+
+if errorlevel 1 exit /b 1
+exit /b 0
